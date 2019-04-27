d0f5987e168d5adeac7cf73f465f0d2a0645c71b
ncsot <ncsot@ya.ru>
2019-04-27 16:03:52 +0300

    automorp number

--- week_1/task_14.c
+++ week_1/task_14.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
+
 
 int main(int argc, char const *argv[])
 {
-	int cntDigit(int cntNumber, int counter) 
+	unsigned long long int userNumber;
+
+	
+	int isAutomorphic(unsigned long long int checkNumber) 
 	{
-		
+		unsigned long long int checkNumberPow = checkNumber * checkNumber;
+		int flag = 0;
+		while(checkNumber > 0) {
+			/*printf("call func. checkNumber = %llu\n", checkNumber);*/
+			if(checkNumber%10 != checkNumberPow%10) {
+				flag = 1;
+				break;
+			}
+			checkNumber /= 10;
+			checkNumberPow /= 10;
+		}
+		return flag;
+	}
+
+	printf("Enter number from 1 to %llu: \n", sqrt(ULLONG_MAX));
+	scanf("%llu", &userNumber);
+	while(userNumber > 0) {
+		if (isAutomorphic(userNumber) == 0)
+		 {
+			printf("automorph numb = %llu\n", userNumber);
+		}
+		userNumber = userNumber - 1;
 	}
 	return 0;
 }
